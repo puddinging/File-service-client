a4d32d2330fd6eb9fb98d0110e4e1a82d02b0c0b
jiefeng <jiefengfire@163.com>
2019-11-14 10:30:36 +0800

    初始化仓库

---
+++ src/main/java/com/fire/fileserviceclient/FileServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.fire.fileserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fire/fileserviceclient/FileServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fire.fileserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
